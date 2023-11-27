86e02a9eb0d1fcefd7fb687a71f6e0440c2f293a
Massidama <massidama@outlook.it>
2023-11-27 15:42:40 +0100

    feat: created user choices

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -2,16 +2,25 @@
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
-// scelta utente
+// scelte utente
 const choice = prompt("Scegli tra paro e disparo");
+console.log(choice);
 
+const numUser = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log("Il numero da te scelto è: " + numUser);
 
+// numero random pc
+const numPc = generateRandom();
+
+console.log("Il numero scelto dal pc è: " + numPc);
 
 
 
 
 // funzione che genera un numero random
-function generateRandom (min, max) {
-    const numRandom = Math.floor(Math.random() * (max-min)) + min;
-    return numRandom;
+function generateRandom() {
+    const num = Math.floor(Math.random() * 5 ) + 1;
+    return num;
 }
+
+
